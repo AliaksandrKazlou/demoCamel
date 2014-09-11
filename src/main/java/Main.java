c3aedb48ea570774808eb2009e6d6bbfb63fb436
Anton Spirchonak <Anton_Spirchonak@epam.com>
2014-09-11 10:31:42 +0300

    Add spring to project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws InterruptedException {
         new ClassPathXmlApplicationContext("context.xml");
+        Thread.sleep(1000);
     }
 }
