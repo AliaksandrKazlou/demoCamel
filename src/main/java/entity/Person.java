dc5f967381685b9e37c6fb36bb520b014b0e7afe
Anton Spirchonak <Anton_Spirchonak@epam.com>
2014-09-15 14:47:28 +0300

    add person entity

---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,22 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = -4327358834773106572L;
+
+    private String name;
+
+    private Integer age;
+
+    public Person (String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString () {
+        return "Person with name " + name + ";Age: " + age + " years old";
+    }
+}
